40f8d5c9f9c8e597264d5afed7a3b95f469e200d
Lucas Carvalho <maracaipelucas@gmail.com>
2022-03-29 22:53:57 -0300

    Fix null promise

--- chess-client/src/client/MatchClient/UseMatchClient.tsx
+++ chess-client/src/client/MatchClient/UseMatchClient.tsx
@@ -1,27 +1,29 @@
 import axios, { AxiosResponse } from "axios";
 import { useCallback } from "react";
-import { Colors, Match } from "../../client/Board/types";
+import { Match } from "../../client/Board/types";
 import { MatchDTO } from "./types";
 import { createNewMatchDTO, mapMatchDTO } from "../Mappers/MatchMappers";
 
 const useMatchClient = () => {
   const baseURL = `${process.env.REACT_APP_LOCAL_URL_API}/Match/`;
 
-  const getMatch = useCallback((reference: string): Promise<Match | null> => new Promise(
+  const getMatch = useCallback((reference: string): Promise<Match> => new Promise(
     (resolve, reject) => {
       axios.get(reference, { baseURL }).then((response: AxiosResponse) => {
         const match = mapMatchDTO(response.data);
+        if (!match) throw new Error("Não foi possível buscar a partida");
         resolve(match);
       }, (response: AxiosResponse) => reject(response));
     },
   ), [baseURL]);
 
-  const createMatch = useCallback((reference: string, playerName: string): Promise<Match | null> => new Promise(
+  const createMatch = useCallback((reference: string, playerName: string): Promise<Match> => new Promise(
     (resolve, reject) => {
       const matchDto: MatchDTO = createNewMatchDTO(playerName, reference);
 
       axios.post("", matchDto, { baseURL }).then((response: AxiosResponse) => {
         const match = mapMatchDTO(response.data);
+        if (!match) throw new Error("Não foi possível criar a partida");
         resolve(match);
       }, (response: AxiosResponse) => reject(response));
     },
